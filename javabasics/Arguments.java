dc43303d17793c467ff2d8c1e41ab292818a1544
Deependra Raj Amit <deependra.raj.amit@gmail.com>
2025-03-06 23:29:40 +0530

    Arguments Practice

---
+++ javabasics/Arguments.java
@@ -0,0 +1,10 @@
+package javabasics;
+
+public class Arguments {
+    public static void main(@org.jetbrains.annotations.NotNull String[] strings) {
+        System.out.println("Hello World");
+        System.out.println(strings[0]);
+        System.out.println(strings[1]);
+
+    }
+}
